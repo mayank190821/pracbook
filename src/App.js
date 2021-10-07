60ed396b883a7dd40b660da2b1c41259f2ecf9f2
rupin-1801 <rupinbhugra1411@gmail.com>
2021-10-07 16:12:03 +0530

    added show password and value input functionality in login page, added app router to route pages, connected landing page and login page with router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import '../src/styles/App.css';
 import React from 'react';
-import LandingPage from './components/landingPage';
-// import LoginPage from "./components/loginPage";
+import AppRouter from "./components/AppRouter";
 import "../src/styles/App.css";
 
 function App() {
   return (
     <React.Fragment>
-      <LandingPage/>
-      {/* <LoginPage /> */}
+      <AppRouter/>
     </React.Fragment>
   );
 }
