63d632edecba88bea97d622c1502407ac3e3ea50
mudit <muditshukla010@gmail.com>
2021-10-09 14:59:14 +0530

    Added sidebar layout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import LandingPage from './components/landingPage';
 // import LoginPage from "./components/loginPage";
 import "../src/styles/App.css";
+import SideBar from './components/Faculty/SideBar'
 
 function App() {
   return (
     <React.Fragment>
-      <LandingPage/>
+      {/* <LandingPage/> */}
+    <SideBar></SideBar>
       {/* <LoginPage /> */}
     </React.Fragment>
   );
