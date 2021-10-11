33c03b4a8ee5d23215a0d4583a44199471bd982a
mayank190821 <mayank.123.2000@gmail.com>
2021-10-11 21:46:33 +0530

    added some page links

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import '../src/styles/App.css';
 import React from 'react';
 import AppRouter from "./components/AppRouter";
 import "../src/styles/App.css";
-import SideBar from './components/Faculty/SideBar'
+import SideBar from './components/Faculty/SideBar';
+import StudentInfo from './components/Faculty/StudentInfo';
+import StudentTable from './components/Faculty/StudentTable';
 
 function App() {
   return (
     <React.Fragment>
       <AppRouter/>
+      {/* <SideBar/> */}
+      {/* <StudentInfo/> */}
+      {/* <StudentTable/> */}
     </React.Fragment>
   );
 }
