5474452b7a2617e5296d6cbf4f7d3ebb4cc976e8
Harsh Gautam <harsh.gautam_cs19@gla.ac.in>
2021-10-13 14:47:34 +0530

    Completed dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import '../src/styles/App.css';
-import React from 'react';
+import "../src/styles/App.css";
+import React from "react";
 import AppRouter from "./components/AppRouter";
 import "../src/styles/App.css";
-import SideBar from './components/Faculty/SideBar';
-import StudentInfo from './components/Faculty/StudentInfo';
-import StudentTable from './components/Faculty/StudentTable';
+import SideBar from "./components/Faculty/SideBar";
+import StudentInfo from "./components/Faculty/StudentInfo";
+import StudentTable from "./components/Faculty/StudentTable";
+import DashBoard from "./components/Faculty/DashBoard";
 
 function App() {
   return (
     <React.Fragment>
-      <AppRouter/>
+      <DashBoard />
+
+      {/* <AppRouter/> */}
+
       {/* <SideBar/> */}
       {/* <StudentInfo/> */}
       {/* <StudentTable/> */}
