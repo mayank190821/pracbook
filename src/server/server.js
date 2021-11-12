4c656ec5defb70273516159aa64a246e0d88f2fd
rupin-1801 <rupinbhugra1411@gmail.com>
2021-11-12 23:06:02 +0530

    added controller for adding coding question

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 import config from "./../config/config.js";
 import express from "express";
 import mongoose from "mongoose";
+import routes from "./routes/routes.js";
 
 const app = express();
 
@@ -8,20 +9,13 @@ mongoose.connect(config.mongourl);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", routes);
 
 mongoose.connection.on("connected", () => {
     console.log("connected to mongo server.");
 })
 mongoose.connection.on("error", (err) => {
     console.log("error:",err);
-})
-
-app.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("done");
-})
-app.get("/", (req, res) => {
-    res.send("return message");
 })
 
 app.listen(3000, () => {
