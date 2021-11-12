4b56357d91b8af1a83aa7d9d53d47db22a11b635
Harsh Gautam <harsh.gautam_cs19@gla.ac.in>
2021-11-12 22:23:53 +0530

    Edited exam controller

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,14 @@
 import config from "./../config/config.js";
 import express from "express";
 import mongoose from "mongoose";
-
+import routes from "./routes/routes.js";
 const app = express();
 
 mongoose.connect(config.mongourl);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/",routes);
 mongoose.connection.on("connected", () => {
     console.log("connected to mongo server.");
 })
