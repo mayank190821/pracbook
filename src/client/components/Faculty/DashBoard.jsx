da89e45cfa24828ae6d08f4c7d3627860162fab5
rupin-1801 <rupinbhugra1411@gmail.com>
2021-11-12 18:49:03 +0530

    updated app.css path in app.js, added model and route for coding problems and connected mongo

--- src/client/components/Faculty/DashBoard.jsx
+++ src/client/components/Faculty/DashBoard.jsx
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import { makeStyles } from "@mui/styles";
 import Card from "@mui/material/Card";
 import Add from "@mui/icons-material/Add";
 import CardList from "./CardList";
 import AddQuestion from "./AddQuestion";
 import ScheduleExam from "./ScheduleExam";
+import SideBar from "./SideBar";
 
 const useStyle = makeStyles((theme) => ({
   years: {
@@ -49,37 +52,41 @@ function DashBoard() {
   };
 
   return (
-    <div>
-      {openQuestionDialog && <AddQuestion handleClose={handleAddQuestionClose}/>}
-      {openScheduleDialog && <ScheduleExam handleClose={handleScheduleClose}/>}
-      <Card className={classes.cardBody}>
-        <div className={classes.header}>
-          <Stack className={classes.years} spacing={2} direction="row">
-            <Button variant="outlined">1st Year</Button>
-            <Button variant="outlined">2nd Year</Button>
-            <Button variant="outlined">3rd Year</Button>
-            <Button variant="outlined">4th Year</Button>
-          </Stack>
-          <Stack className={classes.profileSection} spacing={2} direction="row">
-            <Button variant="standard"
-            className={classes.button}
-          onClick={handleQuestionButtonClick}>
-              <Add />
-              &nbsp; New Question
-            </Button>
-            <Button variant="standard"
-            className={classes.button}
-            onClick={handleScheduleButtonClick}>
-              <Add />
-              &nbsp; Schedule Exam
-            </Button>
-          </Stack>
-        </div>
-        <div>
-          <CardList />
-        </div>
-      </Card>
-    </div>
+    <React.Fragment>
+      <div>
+        {openQuestionDialog && <AddQuestion handleClose={handleAddQuestionClose}/>}
+        {openScheduleDialog && <ScheduleExam handleClose={handleScheduleClose}/>}
+        <Card className={classes.cardBody}>
+          <div className={classes.header}>
+            <Stack className={classes.years} spacing={2} direction="row">
+              <IconButton variant="outlined"><MenuIcon style={{fontSize: "20px"}}/></IconButton>
+              <Button variant="outlined">1st Year</Button>
+              <Button variant="outlined">2nd Year</Button>
+              <Button variant="outlined">3rd Year</Button>
+              <Button variant="outlined">4th Year</Button>
+            </Stack>
+            <Stack className={classes.profileSection} spacing={2} direction="row">
+              <Button variant="standard"
+              className={classes.button}
+            onClick={handleQuestionButtonClick}>
+                <Add />
+                &nbsp; New Question
+              </Button>
+              <Button variant="standard"
+              className={classes.button}
+              onClick={handleScheduleButtonClick}>
+                <Add />
+                &nbsp; Schedule Exam
+              </Button>
+            </Stack>
+          </div>
+          <div>
+            <CardList />
+          </div>
+        </Card>
+      </div>
+      <SideBar/>
+    </React.Fragment>
   );
 }
 export default DashBoard;
