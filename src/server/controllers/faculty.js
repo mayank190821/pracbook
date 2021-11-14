d3c609f77b37a94cf945bdd89c8616311720dc54
mudit <muditshukla010@gmail.com>
2021-11-14 12:13:32 +0530

    added signin and logout facility for faculty

--- src/server/controllers/faculty.js
+++ src/server/controllers/faculty.js
@@ -13,4 +13,39 @@ const createFaculty = async(req, res) => {
     }
 }
 
-export {createFaculty};
+const login = async (req, res) => {
+    try {
+      let user = await facultyModel.findOne({ email: req.body.email });
+      if (!user) return res.status(401).json({ error: "User not found!" });
+      if (!user.authenticate(req.body.password))
+        return res.status(401).json({ error: "Email and password don't match!" });
+  
+      const token = jwt.sign(
+        {
+          _id: user._id,
+        },
+        config.jwtSecret
+      );
+  
+      res.cookie("ft", token, { expire: new Date() + 9999 });
+  
+      return res.status(200).json({
+        token: token,
+        user: user,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+  };
+
+  const logout = (req, res) => {
+      res.clearCookie("ft");
+      return res.status(200).json({
+          message : "Successfully logged out"
+      });
+  }
+
+
+export {createFaculty, login, logout};
