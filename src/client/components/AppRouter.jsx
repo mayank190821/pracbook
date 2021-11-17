94b3bb840a1707976dc7769c335f9bf481844766
Rupin Bhugra <61143327+rupin-1801@users.noreply.github.com>
2021-11-17 19:49:24 +0530

    added signup component

--- src/client/components/AppRouter.jsx
+++ src/client/components/AppRouter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginPage from "./loginPage";
+import SignupPage from "./signupPage";
 import LandingPage from "./landingPage";
 import DashBoard from "./Faculty/DashBoard";
 import { Switch, Route } from "react-router";
@@ -7,9 +8,8 @@ import { Switch, Route } from "react-router";
 export default function AppRouter() {
   return (
     <Switch>
-      <Route path="/login">
-        <LoginPage />
-      </Route>
+      <Route path="/login" component={LoginPage}/>
+      <Route path="/signup" component={SignupPage}/>
       <Route path="/faculty/dashboard" component={DashBoard}/>
       <Route exact path="/" component={LandingPage} />
     </Switch>
