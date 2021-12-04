de2c94ccfdb245a69643edac7421bf1559df31d4
rupin-1801 <rupinbhugra1411@gmail.com>
2021-12-04 19:19:33 +0530

    updated port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connection.on("error", (err) => {
 
 app.use(express.static(path.resolve(__dirname, "./build")));
 
-app.listen(process.env.port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("listening..");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./build", "index.html"));
